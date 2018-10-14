eb38f83561e4b89cbcd16b33a975f2fb5c9e4482
duza <duza@tut.by>
2018-10-14 21:37:10 +0300

    Add a solution from task analysis

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let tempBase = base;
+  let maxNumber = Number.MAX_VALUE;
+  
+  for (let i = 2; i <= tempBase; i++) {
+    if (tempBase % i === 0){
+      let denominator = 0;
+      
+      while (tempBase % i === 0) {
+        tempBase /= i;
+        denominator++;
+      }
+
+      let innerRes = Math.floor(number / i);
+      
+      let count = 0;
+      while (innerRes > 0) {
+        count += innerRes;
+        innerRes = Math.floor(innerRes / i);
+      }
+
+      maxNumber = Math.min(maxNumber, Math.floor(count / denominator));
+    }
+  }
+  return maxNumber;
 }
